044de34d1bca031d97e2f0e98612e8aaf7b0f973
Agustín Ramos <zentimental.software@gmail.com>
2018-12-07 04:01:12 -0600

    Day 6. solution to 2nd part

--- lib/06/chronal_coordinates.ex
+++ lib/06/chronal_coordinates.ex
@@ -2,6 +2,45 @@ defmodule ChronalCoordinates do
   @no_area "_"
   @mult_areas "."
 
+  def safe_region(path, dist_lim) when is_binary(path) do
+    path
+    |> read_input()
+    |> safe_region(dist_lim)
+  end
+
+  def safe_region(coords, dist_lim) do
+    dims = matrix_dims(coords)
+
+    empty_matrix(dims)
+    |> calc_total_dists(coords)
+    |> count_in_range(dims, dist_lim)
+  end
+
+  defp count_in_range(mtx, {min_x, max_x, min_y, max_y}, dist_lim) do
+    Enum.map(min_x..max_x, fn x ->
+      Enum.count(min_y..max_y, fn y ->
+        get_in(mtx, [x, y]) < dist_lim
+      end)
+    end)
+    |> Enum.sum()
+  end
+
+  defp calc_total_dists(mtx, coords) do
+    {min_x, max_x, min_y, max_y} = matrix_dims(coords)
+
+    Enum.reduce(min_x..max_x, mtx, fn x, mtx2 ->
+      Enum.reduce(min_y..max_y, mtx2, fn y, mtx3 ->
+        put_in(mtx3[x][y], total_distance({x, y}, coords))
+      end)
+    end)
+  end
+
+  defp total_distance(orig, coords) do
+    coords
+    |> Enum.map(&distance(orig, &1))
+    |> Enum.sum()
+  end
+
   def largest_area(path) when is_binary(path) do
     path
     |> read_input()
@@ -17,7 +56,7 @@ defmodule ChronalCoordinates do
     |> pick_largest_area(coords)
   end
 
-  def matrix_dims(coords) do
+  defp matrix_dims(coords) do
     min_x = coords |> Enum.map(fn {x, _} -> x end) |> Enum.min
     max_x = coords |> Enum.map(fn {x, _} -> x end) |> Enum.max
     min_y = coords |> Enum.map(fn {_, y} -> y end) |> Enum.min
@@ -25,20 +64,20 @@ defmodule ChronalCoordinates do
     {min_x - 1, max_x + 1, min_y - 1, max_y + 1}
   end
 
-  def empty_matrix({_min_x, max_x, _min_y, max_y}) do
+  defp empty_matrix({_min_x, max_x, _min_y, max_y}) do
     for _ <- 0..max_x do
       for _ <- 0..max_y, do: @no_area
     end
     |> Matrix.from_list()
   end
 
-  def place_coords(mtx, coords) do
+  defp place_coords(mtx, coords) do
     coords
     |> coords_with_names()
     |> Enum.reduce(mtx, fn {{x, y}, name}, mtx -> put_in(mtx[x][y], name) end)
   end
 
-  def assign_areas(mtx, coords) do
+  defp assign_areas(mtx, coords) do
     coords_names = coords |> coords_with_names()
     {min_x, max_x, min_y, max_y} = matrix_dims(coords)
 
@@ -49,7 +88,7 @@ defmodule ChronalCoordinates do
     end)
   end
 
-  def pick_largest_area(mtx, coords) do
+  defp pick_largest_area(mtx, coords) do
     dims = matrix_dims(coords)
 
     edge_areas =
@@ -62,20 +101,20 @@ defmodule ChronalCoordinates do
     |> Enum.max()
   end
 
-  def to_uniq_area_names(values) do
+  defp to_uniq_area_names(values) do
     values
     |> Enum.uniq
     |> Enum.filter(&(&1 != @mult_areas))
     |> Enum.map(&String.upcase/1)
   end
 
-  def area_names(coords) do
+  defp area_names(coords) do
     coords
     |> coords_with_names()
     |> Enum.map(fn {_cord, name} -> name end)
   end
 
-  def edges_values(mtx, {min_x, max_x, min_y, max_y}) do
+  defp edges_values(mtx, {min_x, max_x, min_y, max_y}) do
     edge_1 = Enum.map(min_x..max_x, fn x -> get_in(mtx, [x, min_y]) end)
     edge_2 = Enum.map(min_x..max_x, fn x -> get_in(mtx, [x, max_y]) end)
     edge_3 = Enum.map(min_y..max_y, fn y -> get_in(mtx, [min_x, y]) end)
@@ -83,7 +122,7 @@ defmodule ChronalCoordinates do
     (edge_1 ++ edge_2 ++ edge_3 ++ edge_4)
   end
 
-  def area_size(name, mtx, {min_x, max_x, min_y, max_y}) do
+  defp area_size(name, mtx, {min_x, max_x, min_y, max_y}) do
     name_d = String.downcase(name)
     Enum.reduce(min_x..max_x, 0, fn x, count ->
       Enum.reduce(min_y..max_y, count, fn y, count2 ->
@@ -92,24 +131,24 @@ defmodule ChronalCoordinates do
     end)
   end
 
-  def set_area(val = "C" <> _id, _coord, _coords_names), do: val
-  def set_area("_", {x, y}, coords_names) do
-    distances = Enum.map(coords_names, fn {coord, name} -> {name, distance(coord, {x, y})} end)
-    {_, min_distance} = Enum.min_by(distances, fn {_name, dist} -> dist end)
-    case Enum.filter(distances, fn {_name, dist} -> dist == min_distance end) do
+  defp set_area(val = "C" <> _id, _coord, _coords_names), do: val
+  defp set_area("_", {x, y}, coords_names) do
+    dists = Enum.map(coords_names, fn {c, name} -> {name, distance(c, {x, y})} end)
+    {_, min_d} = Enum.min_by(dists, fn {_name, dist} -> dist end)
+    case Enum.filter(dists, fn {_name, dist} -> dist == min_d end) do
       [{name, _dist} | []] -> String.downcase(name)
       _ -> @mult_areas
     end
   end
 
-  def distance({x1, y1}, {x2, y2}) do
+  defp distance({x1, y1}, {x2, y2}) do
     :erlang.abs(x2 - x1) + :erlang.abs(y2 - y1)
   end
 
-  def coords_with_names(coords) do
+  defp coords_with_names(coords) do
     coords
     |> Enum.with_index
-    |> Enum.map(fn {{x, y}, idx} -> {{x, y}, "C" <> Integer.to_string(idx)} end)
+    |> Enum.map(fn {coord, idx} -> {coord, "C" <> Integer.to_string(idx)} end)
   end
 
   defp read_input(path) do
