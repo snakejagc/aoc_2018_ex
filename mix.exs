2cf10539ead05ce977c0bdd0cac1a3305687d5fd
aurrutia <andrea.urrutia@bunsan.io>
2021-04-06 09:41:39 -0500

    Updated req elixir version to 1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Aoc2018Ex.MixProject do
     [
       app: :aoc_2018_ex,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
